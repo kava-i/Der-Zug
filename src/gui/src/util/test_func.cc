0d30ee01936292063a4f6470eac1a66d52da5e86
fux <jan.dick@bdp.org>
2020-10-28 14:32:03 +0100

    Adds basic tests for func.h

--- src/gui/src/util/test_func.cc
+++ src/gui/src/util/test_func.cc
@@ -3,13 +3,25 @@
  */
 
 #include <iostream>
+#include <string>
 
 #include <catch2/catch.hpp>
 
 #include "func.h"
 
-TEST_CASE ("Loading pages from disc is working", "[get_page]") {
-
+TEST_CASE ("Return to lower", "[to_lower]") {
+  SECTION ("Handle normal use-cases") {
+    std::string str = "HELLO WORLD";
+    REQUIRE(func::ReturnToLower(str) == "hello world");
+    str = "HELLO 1234 WORLD";
+    REQUIRE(func::ReturnToLower(str) == "hello 1234 world");
+    str = "HELLO §$%&/()= WORLD";
+    REQUIRE(func::ReturnToLower(str) == "hello §$%&/()= world");
+  }
+  SECTION ("Handle german specific cases") {
+    std::string str = "HÄLLO WÖRLD";
+    REQUIRE(func::ReturnToLower(str) == "hällo wörld");
+  }
 }
 
 TEST_CASE ("Loading images from disc is working", "[get_page]") {
