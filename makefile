9b9537cd1b4961502d2389e23761890fc019ac06
fux <jan.dick@bdp.org>
2020-10-28 14:11:11 +0100

    .gitignore fix

--- makefile
+++
@@ -1,2 +0,0 @@
-run: 
-	valgrind build/bin/txtadventure
--- src/textadventure/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fux/Documents/programming/Der-Zug/src/textadventure
-# Build directory: /home/fux/Documents/programming/Der-Zug/src/textadventure/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MainTest "../bin/txtadventure" "-d" "true")
-set_tests_properties(MainTest PROPERTIES  _BACKTRACE_TRIPLES "/home/fux/Documents/programming/Der-Zug/src/textadventure/CMakeLists.txt;42;add_test;/home/fux/Documents/programming/Der-Zug/src/textadventure/CMakeLists.txt;0;")
