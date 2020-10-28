df51bba1412df426d71cbea6a696bbd432902f45
fux <jan.dick@bdp.org>
2020-10-28 15:20:21 +0100

    Adds test to register new user

--- src/gui/src/users/test_user_manager.cc
+++ src/gui/src/users/test_user_manager.cc
@@ -2,12 +2,33 @@
  * @author georgbuechner
  */
 
+#include <filesystem>
 #include <iostream>
 
 #include <catch2/catch.hpp>
 
 #include "user_manager.h"
 
-TEST_CASE ("Adding, remove, logging in and out users", "[user_manager]") {
+TEST_CASE ("Registering, loading and removing users works", "[creating]") {
+  //If already exists, delete test user.
+  try {
+    std::filesystem::remove_all ("../../data/test_manager");
+  }
+  catch (...) {}
+  
+  //Create user manager.
+  UserManager user_manager("../../data/users/", {"attacks", 
+    "defaultDialogs", "dialogs", "players", "rooms", "characters", 
+    "defaultDescriptions", "details", "items", "quests", "texts"});
 
+  SECTION ("Registering user works") {
+        REQUIRE(user_manager.DoRegistration("test_manager", "pw", 
+          "pw") == "Strength insufficient!");
+    REQUIRE(user_manager.DoRegistration("test_manager", "password1234", 
+          "password1235") == "Passwords do not match!");
+    REQUIRE(user_manager.DoRegistration("test_manager", "password1234", 
+          "password1234") == "");
+    REQUIRE(user_manager.DoRegistration("test_manager", "password1234", 
+          "password1234") == "Username already exists!");
+  }
 }
