87f0ffea45c729976df717da8f30a592f5ceb686
fux <jan.dick@bdp.org>
2020-10-30 01:56:03 +0100

    Removes a few tests to reduce overload

--- src/gui/src/users/test_user.cc
+++ src/gui/src/users/test_user.cc
@@ -101,18 +101,13 @@ TEST_CASE ("Loading pages from user works", "[user_pages]") {
   REQUIRE(user->AddNewObject(room_path, "test_house") == "");
   REQUIRE(system(command.c_str()) == 0);
   REQUIRE(user->AddNewObject(path+"/attacks/test_attacks","test_attack") == "");
-  REQUIRE(system(command.c_str()) == 0);
   REQUIRE(user->AddNewObject(path+"/dialogs/test_dialogs","test_dialog") == "");
-  REQUIRE(system(command.c_str()) == 0);
   REQUIRE(user->AddNewObject(path+"/characters/test_characters", 
         "test_character") == "");
   REQUIRE(system(command.c_str()) == 0);
   REQUIRE(user->AddNewObject(path+"/items/test_items", "test_item") == "");
-  REQUIRE(system(command.c_str()) == 0);
   REQUIRE(user->AddNewObject(path+"/details/test_details","test_details")=="");
-  REQUIRE(system(command.c_str()) == 0);
   REQUIRE(user->AddNewObject(path+"/quests/test_quests", "test_quest") == "");
-  REQUIRE(system(command.c_str()) == 0);
   REQUIRE(user->AddNewObject(path+"/texts/test_texts", "test_text") == "");
   REQUIRE(system(command.c_str()) == 0);
   REQUIRE(user->AddNewObject(path+"/defaultDescriptions/test_default_descs",
