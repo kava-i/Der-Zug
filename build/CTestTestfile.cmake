a4f9332abffa97a8ce5e1ec9398ed8f4188eb5f1
fux <jan.dick@bdp.org>
2020-11-12 18:31:56 +0100

    gitignore fix

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fux/Documents/programming/Der-Zug
-# Build directory: /home/fux/Documents/programming/Der-Zug/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MainTest "../bin/txtadventure" "-d" "true")
-set_tests_properties(MainTest PROPERTIES  _BACKTRACE_TRIPLES "/home/fux/Documents/programming/Der-Zug/CMakeLists.txt;42;add_test;/home/fux/Documents/programming/Der-Zug/CMakeLists.txt;0;")
