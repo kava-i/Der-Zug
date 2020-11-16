e2b67527b8d088226fa8fb9065e4e8e36a9a9746
fux <jan.dick@bdp.org>
2020-11-16 22:54:27 +0100

    Adds full functionality for backups in javascript (adding, deleting, restoring). Fixxes links from backup-overview

--- src/gui/webserver/web/backup.js
+++ src/gui/webserver/web/backup.js
@@ -1,18 +1,25 @@
 //Sends a request to create a backup to the server
 function create_backup() {
   var path = window.location.pathname;
-  var backup = path.substr(7);
+  var request = new Object();
+  request["user"] = path.split("/")[1];
+  request["world"] = path.split("/")[3];
+
+  console.log("path: ", path);
+  console.log("world: ", request["world"]);
+  console.log("user: ", request["user"]);
+
   var xhttp = new XMLHttpRequest();
   xhttp.open("POST", "/api/create_backup");
-  xhttp.send(backup);
+  xhttp.send(JSON.stringify(request));
   xhttp.onload = function(event){    
     var error_msg = document.getElementById("create_backup_error");
     if (xhttp.status != 200) {
-      alert("Adding backup " + backup + " failed. Please try again later or " 
+      alert("Adding backup " + request["world"] + " failed. Please try again later or " 
         + "contact an admin.");
     }
     else 
-      alert("Successfully added backup: " + backup);
+      alert("Successfully added backup: " + request["world"]);
   }
 }
 
@@ -21,8 +28,13 @@ function restore_backup(backup) {
 
   var path = window.location.pathname;
   var request = new Object;
-  request.world = path.substr(path.indexOf("_")+1);
+  request.user = path.split("/")[1];
   request.backup = backup;
+
+  console.log("path: ", path);
+  console.log("backup: ", request["backup"]);
+  console.log("user: ", request["user"]);
+
   var xhttp = new XMLHttpRequest();
   xhttp.open("POST", "/api/restore_backup");
   xhttp.send(JSON.stringify(request));
@@ -39,9 +51,20 @@ function restore_backup(backup) {
 
 //Delete Backup
 function delete_backup(backup) {
+
+  var path = window.location.pathname;
+  var request = new Object;
+  request.user = path.split("/")[1];
+  request.backup = backup;
+
+  console.log("path: ", path);
+  console.log("backup: ", request["backup"]);
+  console.log("user: ", request["user"]);
+
+
   var xhttp = new XMLHttpRequest();
   xhttp.open("POST", "/api/delete_backup");
-  xhttp.send(backup);
+  xhttp.send(JSON.stringify(request));
   xhttp.onload = function(event){    
     var error_msg = document.getElementById("create_backup_error");
     if (xhttp.status != 200) {
